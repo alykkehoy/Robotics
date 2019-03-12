d32a3cc5d60f5262cf27e22e3b45c0cf2134e187
anders <anderslykkehoy@gmail.com>
2019-03-12 19:04:08 -0400

    first version of pid controller

--- Robotics/src/Lab02.java
+++ Robotics/src/Lab02.java
@@ -9,14 +9,17 @@ import lejos.hardware.sensor.NXTUltrasonicSensor;
 
 public class Lab02 {
 	
-	private static NXTUltrasonicSensor ultrasonic_front = new NXTUltrasonicSensor(SensorPort.S2);
-	private static NXTUltrasonicSensor ultrasonic_side = new NXTUltrasonicSensor(SensorPort.S1);
+	private static NXTUltrasonicSensor ultrasonic_front = new NXTUltrasonicSensor(SensorPort.S1);
+	private static NXTUltrasonicSensor ultrasonic_side = new NXTUltrasonicSensor(SensorPort.S2);
 	
 	public static void main(String[] args) {
-		EV3LargeRegulatedMotor motor_r = new EV3LargeRegulatedMotor(MotorPort.A);
-		EV3LargeRegulatedMotor motor_l = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor motor_r = new EV3LargeRegulatedMotor(MotorPort.D);
+		EV3LargeRegulatedMotor motor_l = new EV3LargeRegulatedMotor(MotorPort.A);
 		motor_r.forward();
 		motor_l.forward();
+		
+		double motor_r_speed = 150;
+		double motor_l_speed = 150;
 		
 		final SampleProvider test_front = ultrasonic_front.getDistanceMode();
 		final SampleProvider test_side = ultrasonic_side.getDistanceMode();
@@ -26,30 +29,55 @@ public class Lab02 {
 		
 		float distance_front = 0;
 		float distance_side = 0;
+		
+		double kp = 1.2;
+		double ki = 0.0008; 
+		double kd = 5;
+		
+		double integral_front = 0;
+		double integral_side = 0;
+		
+		float last_error_front = 0;
+		float last_error_side = 0;
 
-		while(true){
+		while(true){			
 			test_front.fetchSample(sample_front, 0);
 			test_side.fetchSample(sample_side, 0);
 
 			float error_front = distance_front - sample_front[0];
 			float error_side = distance_side - sample_side[0];
+			
+			float derivative_front = error_front - last_error_front;
+			float derivative_side = error_side - last_error_side;
+			
+			integral_front = integral_front + error_front;
+			integral_side = integral_side + error_side;
+			
+			double correction_front = kp * error_front + ki * integral_front + kd * derivative_front;
+			double correction_side = kp * error_side + ki * integral_side + kd * derivative_side;
 
-			motor_r.setSpeed(0);
-			motor_l.setSpeed(0);
+			double motor_r_turn = 20 - correction_side;
+			double motor_l_turn = 20 + correction_side;
+			
+			motor_r.setSpeed((int)(motor_r_speed + motor_r_turn));
+			motor_l.setSpeed((int)(motor_l_speed + motor_l_turn));
+			
+			last_error_front = error_front;
+			last_error_side = error_side;
 		}
 	}
 	
-	public float calc_p(){
-		float p_val = 0;
+	public float calc_p(double kp, float error){
+		float p_val = (float)(kp * error);
 		return p_val;
 	}
 	
-	public float calc_i(){
+	public float calc_i(double ki, float error){
 		float i_val = 0;
 		return i_val;
 	}
 	
-	public float calc_d(){
+	public float calc_d(double kd, float error){
 		float d_val = 0;
 		return d_val;
 	}
