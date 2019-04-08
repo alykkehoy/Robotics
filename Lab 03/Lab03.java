8e672b552981dc2788f3d097f70a40d89ef20a98
Christian Isolda <christian.isolda1@marist.edu>
2019-04-08 13:33:36 -0400

    Updated Speeds and motors

--- Lab 03/Lab03.java
+++ Lab 03/Lab03.java
@@ -1,9 +1,9 @@
 //import lejos.robotics.RegulatedMotor;
 //import lejos.robotics.SampleProvider;
 //import lejos.utility.Delay;
-//import lejos.hardware.motor.EV3LargeRegulatedMotor;
-//import lejos.hardware.port.MotorPort;
-//import lejos.hardware.port.Port;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
 
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.NXTUltrasonicSensor;
@@ -13,14 +13,14 @@ import lejos.robotics.subsumption.Behavior;
 
 public class Lab03 {
 	
-	static NXTUltrasonicSensor ultrasonic_left = new NXTUltrasonicSensor(SensorPort.S1);
-	static NXTUltrasonicSensor ultrasonic_right = new NXTUltrasonicSensor(SensorPort.S2);
+	static NXTUltrasonicSensor ultrasonic_left = new NXTUltrasonicSensor(SensorPort.S4);
+	static NXTUltrasonicSensor ultrasonic_right = new NXTUltrasonicSensor(SensorPort.S1);
 	
 	public static void main(String[] args) {
 		Behavior b1 = new UltrasonicAvoid(ultrasonic_left, ultrasonic_right);
 		Behavior b3 = new Roam();
 		
-		Behavior[] behaviors = {b1, b3};
+		Behavior[] behaviors = {b3, b1};
 		
 		Arbitrator arbitrator = new Arbitrator(behaviors);
 		arbitrator.go();
